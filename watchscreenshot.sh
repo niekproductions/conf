41942c71ad80dec6344ec3f9235402a5cf6f3bda
Niek Blankers <niek@niekproductions.com>
2017-09-11 15:31:03 +0200

    Add sleep and notification to watchscreenshot.sh

--- watchscreenshot.sh
+++ watchscreenshot.sh
@@ -4,11 +4,10 @@ UPLOAD_DIR="https://domain.name/"
 inotifywait -m /home/niek/Pictures/Screenshots -e create -e moved_to |
 while read path action file; do
 	FULLPATH=$path$file
-	#echo "The file '$file' appeared in directory '$path' via '$action'"
-	#echo "New file, uploading from '$FULLPATH'"
-	#mintupload nefas $FULLPATH
+	sleep 1
 	`sshpass -p "password" scp $FULLPATH user@host:path/`
 
 	FILENAME=${FULLPATH##*/}
 	echo -n $UPLOAD_DIR$FILENAME | xclip -sel clip
+	notify-send -u low -t 2000 "Copied '$UPLOAD_DIR$FILENAME' to clipboard"
 done
